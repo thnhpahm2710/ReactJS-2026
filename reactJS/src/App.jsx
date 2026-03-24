c9e10361aafe00f26f54f7403df76f1d07b991c6
Phạm Đức Thành <thnhpahm2710@gmail.com>
2026-03-24 17:56:34 +0700

    fix: title

--- reactJS/src/App.jsx
+++ reactJS/src/App.jsx
@@ -1,19 +1,19 @@
 import './App.css'
-import Table from './components/Bai1/Table'
-import Toggle from './components/Bai2/Toggle'
+import Bai1 from './components/Bai1/Bai1'
+import Bai2 from './components/Bai2/Bai2'
 
 function App() {
 
-    const posts = [
-        {id: 1, title: "Domenic", body: "dcode"},
-        {id: 2, title: "Sally", body: "Students"},
-        {id: 3, title: "Lin", body: "dcode"},
-    ]
+  const posts = [
+    { id: 1, title: "Domenic", body: "dcode" },
+    { id: 2, title: "Sally", body: "Students" },
+    { id: 3, title: "Lin", body: "dcode" },
+  ]
 
   return (
     <>
-        <Table data={posts} />
-        <Toggle />
+      <Bai1 data={posts} />
+      <Bai2 />
     </>
   )
 }
